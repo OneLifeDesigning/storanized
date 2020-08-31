4fda278b4973a3be57e9691a192d3b876fa320e3
Old <onelifedesigning@gmail.com>
2020-08-31 12:50:55 +0200

    [Task: Do SASS: Include automatic procesor sass]

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ app.use(cookieParser())
 app.use(session)
 app.use(sessionMiddleware.getCurrentUser)
 
+if (process.env.NODE_ENV === 'staging' || process.env.NODE_ENV === 'production') {
+	// If staging or production, compile the SASS files
+	require('./lib/production/prepareProduction')();
+}
+else {
+
+	// Also include the CSS routes for on-the-fly compiling
+	const css = require('./config/css.config');
+	app.use('/css', css);
+}
 
 /**
  * View engine setup
@@ -32,6 +42,25 @@ app.set('view engine', 'hbs')
  */
 const router = require('./config/routes.js')
 app.use('/', router)
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Storanized project running on port 3000 - http://localhost:3000/ 🧳🧳🧳🧳🧳')
