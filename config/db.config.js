a155e725605f0d3d4815bd6d14f4c9ee90840314
Old <onelifedesigning@gmail.com>
2020-09-14 17:15:44 +0200

    fake on dependences

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/storanized';
-
+console.log(process.env.MONGODB_URI);
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false  })
   .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
   .catch(error => console.error(`An error ocurred trying to connect to de database ${MONGODB_URI}`, error));
